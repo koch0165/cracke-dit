416ff59af1753305841de19ada0706dd96994ac2
Koushik Chennugari <koushik.chennugari@cohesity.com>
2019-05-08 16:18:59 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cracke-dit",
+    version="1.0",
+    description="library to crack open the NTDS.DIT",
+    author_email="koch0165@colorado.edu",
+    url="",
+    keywords=["cracke-dit", "NTDS.DIT"],
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="library to crack open the NTDS.DIT."
+)
+
